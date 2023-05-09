f9bce97ce2a593ebe6684f529a42d0ba63eb5d16
TynysA <jjakellucas@gmail.com>
2023-05-10 04:55:28 +0600

    first commit

---
+++ src/main/java/com/arjuncodes/studentsystem/service/StudentServiceImpl.java
@@ -0,0 +1,30 @@
+package com.arjuncodes.studentsystem.service;
+
+import com.arjuncodes.studentsystem.model.Student;
+import com.arjuncodes.studentsystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public void deleteStudentById(int id) {
+        studentRepository.deleteById(id);
+    }
+}
